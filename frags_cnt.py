653565538dc2808614de6700912ea419482219e2
bdnguye2 <47444652+bdnguye2@users.noreply.github.com>
2019-02-08 21:23:09 -0800

    Update frags_cnt.py
    Edit the script since monomerB data is not needed

--- frags_cnt.py
+++ frags_cnt.py
@@ -4,7 +4,7 @@
 # computes the total charge of fragments from
 # Mulliken population analysis of the supermolecules.
 # The csv file must be formatted with the following
-# columns: Supermolecule, Monomer A, Monomer B. 
+# columns: Supermolecule, and Monomer A. 
 # In addition, the supermolecule coordinates must 
 # have atoms organized starting with Monomer A 
 # then Monomer B.
@@ -20,20 +20,16 @@ filename = input('Enter the filename: ')
 with open(filename,'r') as csv_file:
      mylist = []
      mylist2= []
-     mylist3= []
      for i in csv.reader(csv_file, delimiter=','):
          mylist.append(i[0])
          mylist2.append(i[1])
-         mylist3.append(i[2])
      mylist2 = list(filter(None, mylist2))
-     mylist3 = list(filter(None, mylist3))
 
 tmp=0
 for i in mylist[1:len(mylist2)]:
     tmp += float(i)
 
 tmp2=0
-tot = len(mylist2)+len(mylist3)
 for i in mylist[len(mylist2):]:
     tmp2 += float(i)
 
